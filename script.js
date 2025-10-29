c7eaafce6cb2eae1dc43b380eb7fe77bab8531fd
MrActividades854 <kevinjoelescobar2020@gmail.com>
2025-10-28 22:26:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const elVideo = document.getElementById('video');
 
 navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
 
+const cargarCamera = () => {
+    navigator.getMedia (
+
+        {
+            video: true,
+            audio: false
+        },
+
+        stream => elVideo.strObject = stream,
+        console.error
+
+        function(err){
+            console.log("Ocurrió el siguiente error: " + err)
+        }
+    );
+}
